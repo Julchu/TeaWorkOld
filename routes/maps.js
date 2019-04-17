d89026c998c46c959a8c2920d66145b20b6ce1ab
Julian <jchumtl@gmail.com>
2019-04-17 12:31:00 -0400

    Attempting to implement Google Maps

--- routes/maps.js
+++ routes/maps.js
@@ -1,9 +1,34 @@
+"use strict";
+
+let express = require('express');
+let router = express.Router();
+
 let uri = "";
 if (typeof process.env.PLACES_URI == 'undefined') {
-	uri = "AIzaSyB5onXIIm_aoftt4QQnyXRsy8LevJ7oCyw";
+	uri = "AIzaSyBvfLFLuou2MsbXue_o7v7ws3yXrYvtfxg";
 } else {
 	uri = process.env.PLACES_URI;
 }
+
+function initMap() {
+	let map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+}
+
+router.get("/", async function(req, res, next) {
+	res.render("maps", {
+		title: "Maps", 
+		// cafeTypes: ["Café", "Restaurant", "Other"],
+		uri: uri
+		// initMap: initMap()
+	});
+});
+
+module.exports = router;
+
+// https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap
 
 // https://maps.googleapis.com/maps/api/place/nearbysearch/json
 //   ?location=-33.8670522,151.1957362
--- routes/submit.js
+++ routes/submit.js
@@ -2,14 +2,15 @@
 
 let express = require('express');
 let router = express.Router();
-let cafeSchema = require("./cafes");
+let cafes = require("./cafes");
+let cafeSchema = cafes.cafeSchema;
 
 router.get("/", async function(req, res, next) {
 	console.log(cafeSchema.paths.type.enumValues)
 	res.render("submit", {
 		title: "New Café", 
 		cafeTypes: ["Café", "Restaurant", "Other"]
-	})
+	});
 });
 
 module.exports = router;
