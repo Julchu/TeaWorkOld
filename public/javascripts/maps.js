a668256633d84e9d068fdebe60b7c92649212ae4
julchu <jchumtl@gmail.com>
2019-04-19 01:28:10 -0400

    New map display style testing, added current location marker

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,23 +1,36 @@
 "use strict";
 
-var map, infoWindow;
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
+	let map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: -34.397, lng: 150.644},
 		zoom: 18
 	});
 
-	infoWindow = new google.maps.InfoWindow;
+	let infoWindow = new google.maps.InfoWindow;
 
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
-		var pos = {
+		let pos = {
 			lat: position.coords.latitude,
 			lng: position.coords.longitude
 		};
+
+		// Marker on current location
+		let marker = new google.maps.Marker({
+			position: pos,
+			map: map, 
+			title: "You are here."
+			
+			// icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+   //                                                  new google.maps.Size(22,22),
+   //                                                  new google.maps.Point(0,18),
+   //                                                  new google.maps.Point(11,11)),
+		})
 			infoWindow.setPosition(pos);
 			infoWindow.setContent('You are here.');
 			infoWindow.open(map);
+
+			// Center map on current location
 			map.setCenter(pos);
 		}, function() {
 			handleLocationError(true, infoWindow, map.getCenter());
