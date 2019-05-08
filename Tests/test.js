c40d5b146394f02c2422e1c8ce7b15e1cfb2ba2a
Julian <jchumtl@gmail.com>
2019-05-08 16:06:01 -0400

    Adding test files

---
+++ Tests/test.js
@@ -0,0 +1,11 @@
+// window.onload = function() {
+// 	var parent = document.getElementById('container1');
+// 	var child = document.getElementById('container2');
+// 	child.style.padding = child.offsetWidth - child.clientWidth + "px";
+// }
+
+function load() {
+	var parent = document.getElementById('container1');
+	var child = document.getElementById('container2');
+	child.style.padding = child.offsetWidth - child.clientWidth + "px";
+}
